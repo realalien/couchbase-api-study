5c6d2b4b5ddce18555eb56ebc100c120ed5aa791
realalien <realalien@gmail.com>
2012-02-21 19:38:07 +0800

    daily update, still experimenting and domain study

---
+++ StudyCouchDB/experimentals/SurveyCreator/SurveyCreationDashboardViewController.h
@@ -0,0 +1,13 @@
+//
+//  SurveyCreationDashboardViewController.h
+//  StudyCouchDB
+//
+//  Created by d d on 12-2-21.
+//  Copyright (c) 2012年 d. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SurveyCreationDashboardViewController : UIViewController
+
+@end
