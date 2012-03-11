3713a11688b201e6b9e33fab90a4027326dc8af2
Zhu Jia Cheng <realalien@gmail.com>
2012-03-12 00:04:15 +0800

    experimenting

---
+++ StudyCouchDB/experimentals/NewsHacking/NewsRetriever.m
@@ -0,0 +1,17 @@
+//
+//  NewsRetriever.m
+//  StudyCouchDB
+//
+//  Created by realalien on 12-3-11.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "NewsRetriever.h"
+
+@implementation NewsRetriever
+
+
+
+
+
+@end
---
+++ StudyCouchDB/experimentals/NewsHacking/ProfileSlideCardView.h
@@ -0,0 +1,13 @@
+//
+//  ProfileSlideCardView.h
+//  StudyCouchDB
+//
+//  Created by realalien on 12-3-12.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProfileSlideCardView : UIView
+
+@end
