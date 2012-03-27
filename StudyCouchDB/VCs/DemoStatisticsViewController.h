51dbbec2ebc2797e1fab2aa60fd2857d290bbc62
realalien <realalien@gmail.com>
2012-03-27 09:25:28 +0800

    added placeholder files for statistics

---
+++ StudyCouchDB/VCs/DemoStatisticsViewController.h
@@ -0,0 +1,13 @@
+//
+//  DemoStatisticsViewController.h
+//  StudyCouchDB
+//
+//  Created by d d on 12-3-27.
+//  Copyright (c) 2012年 d. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DemoStatisticsViewController : UIViewController
+
+@end
