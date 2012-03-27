27e83256f4a56f15c033b41ef386cdad07e2e6dc
realalien <realalien@gmail.com>
2012-03-27 18:18:50 +0800

    daily upload for homework

---
+++ StudyCouchDB/Models/Representative.h
@@ -0,0 +1,17 @@
+//
+//  Representative.h
+//  StudyCouchDB
+//
+//  Created by d d on 12-3-27.
+//  Copyright (c) 2012年 d. All rights reserved.
+//
+
+// NOTE:
+//  model after the demo project, Callout Demo from http://goo.gl/8OTHu 
+
+#import <Foundation/Foundation.h>
+
+@interface Representative : NSObject
+
+
+@end
---
+++ StudyCouchDB/Models/Representative.m
@@ -0,0 +1,13 @@
+//
+//  Representative.m
+//  StudyCouchDB
+//
+//  Created by d d on 12-3-27.
+//  Copyright (c) 2012年 d. All rights reserved.
+//
+
+#import "Representative.h"
+
+@implementation Representative
+
+@end
