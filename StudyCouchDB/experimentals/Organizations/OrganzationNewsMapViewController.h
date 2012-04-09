e89101a0c1f5bd8c7eae43f39addc8db10333e7c
realalien <realalien@gmail.com>
2012-04-09 18:09:40 +0800

    daily update

---
+++ StudyCouchDB/experimentals/Organizations/OrganzationNewsMapViewController.h
@@ -0,0 +1,13 @@
+//
+//  OrganzationNewsMapViewController.h
+//  StudyCouchDB
+//
+//  Created by d d on 12-4-9.
+//  Copyright (c) 2012年 d. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OrganzationNewsMapViewController : UIViewController
+
+@end
